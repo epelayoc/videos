8d45c3596724d32ebf65204bb051f624729b35ce
epelayoc <97033678+epelayoc@users.noreply.github.com>
2022-05-26 15:50:32 +0200

    Update videos_cdti.py

--- videos_cdti.py
+++ videos_cdti.py
@@ -1,10 +1,25 @@
+import sqlite3
+import pandas as pd
 import streamlit as st
+from PIL import Image
 
-st.title("Partner search tool")
 
-# Select country
-countries = ['ES', 'FR', 'DE']
-ct = {'ES': 'Spain', 'DE': 'Germany', 'FR':'France'}
-country = st.selectbox('Select country',countries)
-st.write(f'You selected: {country}-{ct[country]}')
+# Title
+image = Image.open('he.jpg')
+st.image(image)
+st.title("Vídeos CDTI de Horizonte Europa")
 
+
+grupos = ['Clusters', 'General', 'Horizonte_Europa', 'Pilar_1', 'Pilar_3', 'Propuesta', 'Transversal']
+
+grupo = st.selectbox('Selecione grupo de vídeos',grupos)
+# grupo = 'Clusters'
+
+conn = sqlite3.connect('videos.db')
+df = pd.read_sql(f'SELECT * FROM videos WHERE videos.Grupo == "{grupo}"', conn)
+conn.close()
+
+
+# tabla
+st.subheader(f'Vídeos de "{grupo}"')
+st.dataframe(df)
