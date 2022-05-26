f3196db2c3fca74ded27777d39a07493f3086d5c
epelayoc <97033678+epelayoc@users.noreply.github.com>
2022-05-26 10:54:24 +0200

    Add files via upload

---
+++ videos.py
@@ -0,0 +1,32 @@
+
+import sqlite3
+import pandas as pd
+import streamlit as st
+from PIL import Image
+
+
+# Title
+image = Image.open('he.jpg')
+st.image(image)
+st.title("Vídeos CDTI de Horizonte Europa")
+
+
+
+grupos = ['Clusters', 'General', 'Horizonte_Europa', 'Pilar_1', 'Pilar_3', 'Propuesta', 'Transversal']
+
+grupo = st.selectbox('Selecione grupo de vídeos',grupos)
+# grupo = 'Clusters'
+
+conn = sqlite3.connect('videos.db')
+df = pd.read_sql(f'SELECT * FROM videos WHERE videos.Grupo == "{grupo}"', conn)
+conn.close()
+
+
+
+
+# tabla
+st.subheader(f'Vídeos de "{grupo}"')
+st.dataframe(df)
+
+
+
