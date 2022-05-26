e5796da2b2ff1cb6b41229f37839a9fbc1db6e00
epelayoc <97033678+epelayoc@users.noreply.github.com>
2022-05-26 10:49:38 +0200

    Update cdti.py

--- cdti.py
+++ cdti.py
@@ -3,26 +3,19 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-
 # Title
 image = Image.open('he.jpg')
 st.image(image)
 st.title("Vídeos CDTI de Horizonte Europa")
 
+grupos = ['Clusters', 'General', 'Horizonte_Europa', 'Pilar_1', 'Pilar_3', 'Propuesta', 'Transversal']
+grupo = st.selectbox('Selecione grupo de vídeos',grupos)
+# grupo = 'Clusters'
 
 conn = sqlite3.connect('videos.db')
-df = pd.read_sql('SELECT * FROM videos', conn)
+df = pd.read_sql(f'SELECT * FROM videos WHERE videos.Grupo == "{grupo}"', conn)
 conn.close()
-grupos = ['Clusters', 'General', 'Horizonte_Europa', 'Pilar_1', 'Pilar_3', 'Propuesta', 'Transversal']
-
-grupo = st.selectbox('Selecione grupo de vídeos',grupos)
-#grupo = 'Clusters'
-dfg = df[df.Grupo == grupo].sort_values(by='Area')
-
-dfg = dfg.drop('Grupo', 1)
-
-
 
 # tabla
 st.subheader(f'Vídeos de "{grupo}"')
-st.dataframe(dfg)
+st.dataframe(df)
